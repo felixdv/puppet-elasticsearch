b6f9b3409217f173c01d5016fa196fe79a831e24
Richard Pijnenburg <richard@ispavailability.com>
2014-02-07 21:54:26 +0100

    Update template testing

--- spec/acceptance/003_template_spec.rb
+++ spec/acceptance/003_template_spec.rb
@@ -1,7 +1,93 @@
 require 'spec_helper_acceptance'
 
-require_relative './json_docs.rb'
+good_json='{
+  "template" : "logstash-*",
+  "settings" : {
+    "index.refresh_interval" : "5s",
+    "analysis" : {
+      "analyzer" : {
+        "default" : {
+          "type" : "standard",
+          "stopwords" : "_none_"
+        }
+      }
+    }
+  },
+  "mappings" : {
+    "_default_" : {
+       "_all" : {"enabled" : true},
+       "dynamic_templates" : [ {
+         "string_fields" : {
+           "match" : "*",
+           "match_mapping_type" : "string",
+           "mapping" : {
+             "type" : "multi_field",
+               "fields" : {
+                 "{name}" : {"type": "string", "index" : "analyzed", "omit_norms" : true },
+                 "raw" : {"type": "string", "index" : "not_analyzed", "ignore_above" : 256}
+               }
+           }
+         }
+       } ],
+       "properties" : {
+         "@version": { "type": "string", "index": "not_analyzed" },
+         "geoip"  : {
+           "type" : "object",
+             "dynamic": true,
+             "path": "full",
+             "properties" : {
+               "location" : { "type" : "geo_point" }
+             }
+         }
+       }
+    }
+  }
+}
+'
 
+bad_json='{
+  "settings" : {
+    "index.refresh_interval" : "5s",
+    "analysis" : {
+      "analyzer" : {
+        "default" : {
+          "type" : "standard",
+          "stopwords" : "_none_"
+        }
+      }
+    }
+  },
+  "mappings" : {
+    "_default_" : {
+       "_all" : {"enabled" : true},
+       "dynamic_templates" : [ {
+         "string_fields" : {
+           "match" : "*",
+           "match_mapping_type" : "string",
+           "mapping" : {
+             "type" : "multi_field",
+               "fields" : {
+                 "{name}" : {"type": "string", "index" : "analyzed", "omit_norms" : true },
+                 "raw" : {"type": "string", "index" : "not_analyzed", "ignore_above" : 256}
+               }
+           }
+         }
+       } ],
+       "properties" : {
+         "@version": { "type": "string", "index": "not_analyzed" },
+         "geoip"  : {
+           "type" : "object",
+             "dynamic": true,
+             "path": "full",
+             "properties" : {
+               "location" : { "type" : "geo_point" }
+             }
+         }
+       }
+    }
+  }
+}
+'
 
 describe "elasticsearch template define:" do
 
@@ -26,8 +112,7 @@ describe "elasticsearch template define:" do
     it 'run should fail' do
       pp = "elasticsearch::template { 'foo': ensure => 'present', file => 'puppet:///modules/another/bad.json' }"
 
-      # Run it twice and test for idempotency
-      expect(apply_manifest(pp, :catch_failures => true).exit_code).to match(error)
+      apply_manifest(pp, :expect_failures => true)
     end
 
   end
